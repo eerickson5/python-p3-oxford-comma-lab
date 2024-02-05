970778d1778babb96e8aa17e829fde034e6a0fd7
Elise Erickson <elisece717@gmail.com>
2024-02-05 08:18:08 -0500

    done

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,10 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+        return " and ".join(items)
+    else:
+        returner = ", ".join(items)
+        last_comma_index = returner.rfind(",")
+        returner = f"{returner[0: last_comma_index+1]} and {returner[last_comma_index + 2: len(returner)]}"
+        return returner
